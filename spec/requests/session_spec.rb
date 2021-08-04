15d1ec44cdc1e873a1b558425e2a5f248317c6b0
toshiro-koba <kobato.231.0625@gmail.com>
2021-08-04 20:54:53 +0900

    rubocop #6

--- spec/requests/session_spec.rb
+++ spec/requests/session_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :request do
-  describe "GET /new" do
-    it "responds successfully" do
+RSpec.describe 'Sessions', type: :request do
+  describe 'GET /new' do
+    it 'responds successfully' do
       get login_path
       expect(response.status).to eq 200
     end
