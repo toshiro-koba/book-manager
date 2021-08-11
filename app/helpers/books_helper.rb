587998a47ec4ba80289f4ab92403fff4a2e7ab5e
toshiro-koba <kobato.231.0625@gmail.com>
2021-08-11 20:05:41 +0900

    編集時にtagの値を入力済みにした

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module BooksHelper
+  def display_tags(tags)
+    display = ''
+    tags.each do |tag|
+      display += "#{tag.title},"
+    end
+    display.delete_suffix(',')
+  end
 end
